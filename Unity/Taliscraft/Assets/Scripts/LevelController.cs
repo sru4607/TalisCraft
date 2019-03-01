453c17c909c88e6ce37bb44152f1cea24282a8df
sru4607 <sru4607@MSS3110-27>
2019-03-01 14:10:15 -0500

    Just Minor Work

---
+++ Unity/Taliscraft/Assets/Scripts/LevelController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+
+    public int levelCount;
+    public List<GameObject> GameObjects;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(levelCount)
+        {
+            case 1:
+            {
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
+	}
+}
