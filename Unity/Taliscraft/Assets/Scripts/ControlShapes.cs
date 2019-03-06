ef2871798fd5b3acf1ec443d6093f5391a785554
sru4607 <sru4607@g.rit.edu>
2019-03-06 12:36:34 -0500

    Added Assets and started coding how to calculate the end of level

--- Unity/Taliscraft/Assets/Scripts/ControlShapes.cs
+++ Unity/Taliscraft/Assets/Scripts/ControlShapes.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ControlShapes : MonoBehaviour {
-	// Use this for initialization
+    // Use this for initialization
+    List<GameObject> Shapes;
 	void Start () {
 
     }
@@ -23,54 +24,94 @@ public class ControlShapes : MonoBehaviour {
     public void SpawnCircle()
     {
         activeShape = Instantiate(CircleTemplate);
+        AddToList();
     }
 
     public void SpawnTriangle()
     {
         activeShape = Instantiate(TriangleTemplate);
+        AddToList();
     }
 
     public void SpawnDiamond()
     {
         activeShape = Instantiate(DiamondTemplate);
+        AddToList();
     }
 
     public void SpawnHexagon()
     {
         activeShape = Instantiate(HexagonTemplate);
+        AddToList();
     }
 
     public void AddToList()
     {
-        
+        Shapes.Add(activeShape);
+        CheckWin();
     }
 
     public void SelectShape()
     {
-        activeShape = GameObject.Find(EventSystem.current.currentSelectedGameObject.name.Substring(1));
+        //activeShape = GameObject.Find(EventSystem.current.currentSelectedGameObject.name.Substring(1));
     }
 
     public void ScaleUpShape()
     {
         if (activeShape.GetComponent<ApplyTransformation>() != null)
+        {
             activeShape.GetComponent<ApplyTransformation>().ScaleUp();
+            CheckWin();
+        }
     }
 
     public void ScaleDownShape()
     {
         if (activeShape.GetComponent<ApplyTransformation>() != null)
+        {
             activeShape.GetComponent<ApplyTransformation>().ScaleDown();
+            CheckWin();
+        }
     }
 
     public void RotateShape()
     {
         if (activeShape.GetComponent<ApplyTransformation>() != null)
+        {
             activeShape.GetComponent<ApplyTransformation>().RotateObject();
+            CheckWin();
+        }
     }
 
     public void ArrayShape()
     {
         if (activeShape.GetComponent<ApplyTransformation>() != null)
+        {
             activeShape.GetComponent<ApplyTransformation>().Array();
+            CheckWin();
+        }
+    }
+
+    public void Pause()
+    {
+        Debug.Log("Pasued");
+    }
+
+    public bool CheckWin()
+    {
+        bool win = false;
+        
+
+
+        if (win)
+        {
+            Debug.Log("Win");
+            return true;
+        }
+        else
+        {
+            Debug.Log("Not Win");
+            return false;
+        }
     }
 }
