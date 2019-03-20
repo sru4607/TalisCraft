9cc9e5e6602adec6c20cdbbe5954363c01af4ca1
Aaron Reiner (RIT Student) <ajr8498@ad.rit.edu>
2019-03-20 13:31:25 -0400

    Completed Play Button

--- Unity/Taliscraft/Assets/Scripts/MenuControl.cs
+++ Unity/Taliscraft/Assets/Scripts/MenuControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuControl : MonoBehaviour
 {
@@ -17,5 +18,10 @@ public class MenuControl : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene(0);
     }
 }
