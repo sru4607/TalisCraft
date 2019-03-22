3d3f79b7e54560fab51250cba4216d936966a3e7
sunferth <srunferth@carolina.rr.com>
2019-03-22 11:42:21 -0400

    Fixed Major Bugs

---
+++ Unity/Taliscraft/Assets/Scripts/ButtonProperties.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Type of button
+/// </summary>
+public enum ButtonType
+{
+    shape,
+    transformation,
+    none
+}
+/// <summary>
+/// Written by Kesavan Shanmugasundaram
+/// holds basic button information
+/// no known errors
+/// </summary>
+public class ButtonProperties : MonoBehaviour
+{
+    public ButtonType buttonType;
+}
